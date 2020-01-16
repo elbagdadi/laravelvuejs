10682da50f5a08252ca547a514def55e799fda39
Abderrahim EL BAGDADI <elbagdadi.abdou@gmail.com>
2020-01-16 13:45:22 +0100

    la creation de l'entité Post et Category avec les controlleur

---
+++ app/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public  function author(){
+        return $this->belongsTo(User::class);
+    }
+    public  function category(){
+          return $this->belongsTo(Category::class,'cat_id');
+    }
+}
